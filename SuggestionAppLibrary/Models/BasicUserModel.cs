89a971f124bf0cb9e8ad14a577f3b0942c11df9e
ben.li <ben.li@rightcrowd.com>
2022-04-21 16:51:49 +1000

    Complete Model

---
+++ SuggestionAppLibrary/Models/BasicUserModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SuggestionAppLibrary.Models;
+
+public class BasicUserModel
+{
+   [BsonRepresentation(BsonType.ObjectId)] // this is object id,
+   public string Id { get; set; }
+   public string DisplayName { get; set; }
+
+   public BasicUserModel()
+   {
+
+   }
+
+   public BasicUserModel(UserModel user)
+   {
+      Id = user.Id;
+      DisplayName = user.DisplayName;
+   }
+}
--- SuggestionAppLibrary/Models/SuggestionModel.cs
+++ SuggestionAppLibrary/Models/SuggestionModel.cs
@@ -2,10 +2,18 @@
 
 public class SuggestionModel
 {
-    [BsonId] // identifier
-    [BsonRepresentation(BsonType.ObjectId)] // this is object id,
-    public string Id { get; set; }
-    public string Suggestion { get; set; }
-    public string Description { get; set; }
-    public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+   [BsonId] // identifier
+   [BsonRepresentation(BsonType.ObjectId)] // this is object id,
+   public string Id { get; set; }
+   public string Suggestion { get; set; }
+   public string Description { get; set; }
+   public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+   public CategoryModel Category { get; set; }
+   public string Author { get; set; } // <- need change
+   public HashSet<string> UserVotes { get; set; } = new(); // unique value stirng
+   public StatusModel SuggestionStatus { get; set; } // watched, completed, upComming
+   public string OwnerNotes { get; set; }
+   public bool ApprovedForRealease { get; set; } = false; 
+   public bool Archived { get; set; } = false; // been Archived - not include when loading into view
+   public bool Rejected { get; set; } = false;
 }
